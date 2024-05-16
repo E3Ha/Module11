aa83e2b2d11eab150e6dab524e2f846f3066165e
E3Ha <emma.harrison@talawanda.org>
2024-05-16 14:00:12 -0400

    5/16/2024 2:00

--- src/LabFileAway.java
+++ src/LabFileAway.java
@@ -1,17 +1,20 @@
-import java.util.*;
 import java.io.*;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import java.util.Scanner;
 
-public class Lab {
+public class LabFileAway {
     public static void main(String[] args) {
         //Always have this
         Scanner scan;
         File selectedFile;
         JFileChooser chooser = new JFileChooser();
         int count = 0;
+        int numOfWords = 0;
+        int characters = 0;
+        int lines = 0;
+        String fileName = "";
 
         //Always have a try
         try{
@@ -19,29 +22,37 @@ public class Lab {
             File workingDirectory = new File(System.getProperty("user.dir"));
             chooser.setCurrentDirectory(workingDirectory);
             //Always choose a file to examine
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 //Set file to examine
                 selectedFile = chooser.getSelectedFile();
+                fileName = chooser.getName(selectedFile);
                 //Dump contents into scanner
                 scan = new Scanner(selectedFile);
                 //Examine line by line, so long as another line exists
                 boolean found = false;
-                while(scan.hasNextLine()){
+                String[] words = new String[0];
+                //while there is a line...
+                while (scan.hasNextLine()) {
                     String line = scan.nextLine(); //Entire line = string
-                    String words[] = line.split(" ");
-
-                    for(int i = 0; i < words.length; i ++) {
+                    //Calculate amount of characters and lines
+                    characters = characters + line.length();
+                    lines++;
+                    //Split file into individual words && count number of words
+                    words = line.split(" ");
+                    numOfWords = numOfWords + words.length;
+                    //Check for how many times specified word occurs
+                    for (int i = 0; i < words.length; i++) {
                         if (words[i].equalsIgnoreCase("talawanda")) {
                             found = true;
-                            count ++;
+                            count++;
                         }
+
                     }
                 }
                 //If found/not found, output to user
-                if (found == true){
-                    JOptionPane.showMessageDialog(null, "Word Found " + count + " time(s)");
-                }
-                else{
+                if (found == true) {
+                    JOptionPane.showMessageDialog(null, "File: " + fileName + "\nTalawanda found " + count + " time(s)\nLines: " + lines + "\nWords: " + numOfWords + "\nCharacters: " + characters);
+                } else {
                     JOptionPane.showMessageDialog(null, "Word not Found");
 
                 }
@@ -52,13 +63,6 @@ public class Lab {
             JOptionPane.showMessageDialog(null, "File not Found");
             System.exit(0);
         }
-
-
-
-
-
-
-
 
     }
 }
